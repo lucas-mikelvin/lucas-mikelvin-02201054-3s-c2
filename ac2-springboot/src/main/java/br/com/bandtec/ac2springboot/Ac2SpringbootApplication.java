9f605415ddb6f9dfb23f382f195b66c0fe028662
lucas-mikelvin <lucas.matos@bandtec.com.br>
2021-04-16 16:44:37 -0300

    Adicionando projeto gerado no initializr e adicionando pastas para melhor organização dos arquivos

---
+++ ac2-springboot/src/main/java/br/com/bandtec/ac2springboot/Ac2SpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.ac2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac2SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac2SpringbootApplication.class, args);
+	}
+
+}
---
+++ ac2-springboot/src/test/java/br/com/bandtec/ac2springboot/Ac2SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.ac2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac2SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
